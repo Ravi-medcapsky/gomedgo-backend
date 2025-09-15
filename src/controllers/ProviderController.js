7f4852d2fc456b89129f9c291a7c2516072bb97f
unknown <ravikant.medcapsky@gmail.com>
2025-09-15 14:57:17 +0530

    fix import

--- src/controllers/ProviderController.js
+++ src/controllers/ProviderController.js
@@ -1,6 +1,5 @@
 import * as providerService from "../services/providerService.js";
 
-
 export const serviceProvider = async (req, res) => {
   try {
     const {
@@ -13,10 +12,9 @@ export const serviceProvider = async (req, res) => {
       second_language,
     } = req.body;
 
-    console.log('body',req.body);
+    console.log("body", req.body);
 
-    
-    const service_provider_id = req.session.service_provider_id
+    const service_provider_id = req.session.service_provider_id;
     console.log("service_provider_id", service_provider_id);
 
     if (
@@ -28,7 +26,9 @@ export const serviceProvider = async (req, res) => {
       !first_language ||
       !second_language
     ) {
-      return res.status(400).json({status:400, message: "all data are required" });
+      return res
+        .status(400)
+        .json({ status: 400, message: "all data are required" });
     }
     //checking alredy filled or not Personal Data
 
@@ -49,7 +49,12 @@ export const serviceProvider = async (req, res) => {
         first_language,
         second_language,
       });
-      res.status(200).json({status:200, message: "Provider Updated Personal Information successfully" });
+      res
+        .status(200)
+        .json({
+          status: 200,
+          message: "Provider Updated Personal Information successfully",
+        });
     } else {
       const provider = await providerService.providerCreate({
         service_provider_id,
@@ -61,61 +66,67 @@ export const serviceProvider = async (req, res) => {
         first_language,
         second_language,
       });
-      res
-        .status(201)
-        .json({
-          status:201,
-          message: "Service Provider  Personal information save Successfully",
-          provider,
-        });
+      res.status(201).json({
+        status: 201,
+        message: "Service Provider  Personal information save Successfully",
+        provider,
+      });
     }
   } catch (error) {
     console.error("Request  Error:", error);
-    res.status(500).json({status:500, message: "Internal server error" });
+    res.status(500).json({ status: 500, message: "Internal server error" });
   }
 };
-
 
 export const getProvider = async (req, res) => {
   try {
-    const  service_provider_id  = req.session.service_provider_id;
+    const service_provider_id = req.session.service_provider_id;
     const provider = await providerService.fetchProvider(service_provider_id);
 
-    if (!provider  || provider.length === 0) {
-      return res.status(404).json({ 
+    if (!provider || provider.length === 0) {
+      return res.status(404).json({
         status: 404,
         success: false,
-        message: "No provider found " 
+        message: "No provider found ",
       });
     }
 
-    return res.status(200).json({ 
+    return res.status(200).json({
       status: 200,
       success: true,
-      data: provider 
+      data: provider,
     });
   } catch (error) {
     console.error("Error fetching provider photos:", error);
-    return res.status(500).json({ 
+    return res.status(500).json({
       status: 500,
       success: false,
-      message: "Error fetching provider photos", 
-      error: error.message 
+      message: "Error fetching provider photos",
+      error: error.message,
     });
   }
 };
 
-
 export const updateProviderVerified = async (req, res) => {
   try {
-       // add only what should be updated
-       const{ first_name,last_name,email,mobile_no} = req.body;
-       const service_provider_id = req.session.service_provider_id;
+    // add only what should be updated
+    const { first_name, last_name, email, mobile_no } = req.body;
+    const service_provider_id = req.session.service_provider_id;
 
-    
-
-    const updated = await providerService.updateVerifiedProvider({ service_provider_id,  first_name,last_name,email,mobile_no });
-    res.status(200).json({ status:200,message: 'Provider updated successfully', data: updated });
+    const updated = await providerService.updateVerifiedProvider({
+      service_provider_id,
+      first_name,
+      last_name,
+      email,
+      mobile_no,
+    });
+    res
+      .status(200)
+      .json({
+        status: 200,
+        message: "Provider updated successfully",
+        data: updated,
+      });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -123,11 +134,13 @@ export const updateProviderVerified = async (req, res) => {
 
 export const deleteProvider = async (req, res) => {
   try {
-      const service_provider_id = req.session.service_provider_id;
+    const service_provider_id = req.session.service_provider_id;
 
     await providerPhotoServices.deleteProviderPhoto({ service_provider_id });
-    res.status(200).json({status:200,message: 'Photo deleted successfully' });
+    res
+      .status(200)
+      .json({ status: 200, message: "Photo deleted successfully" });
   } catch (error) {
-    res.status(500).json({status:500, error: error.message });
+    res.status(500).json({ status: 500, error: error.message });
   }
 };
--- src/routes/provider.routes.js
+++ src/routes/provider.routes.js
@@ -1,10 +1,12 @@
-import express from "express";
-import { providerController } from "../controllers/providerController.js";
+import express from 'express';
+import * as providerController from '../controllers/providerController.js';
+
+
 
 const router = express.Router();
 
-router.post("/register/serviceProvider", providerController.serviceProvider);
-router.get("/fetch-provider", providerController.getProvider);
-router.put("/update-provider", providerController.updateProviderVerified);
+router.post('/register/serviceProvider', providerController.serviceProvider);
+router.get('/fetch-provider',providerController.getProvider)
+router.put('/update-provider',providerController.updateProviderVerified)
 
 export default router;
